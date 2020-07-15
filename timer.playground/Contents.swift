af70248799569c4239c1446769b03c3b58f770b9
emily-gutierrez <67969028+emily-gutierrez@users.noreply.github.com>
2020-07-15 10:41:06 -0500

    Add files via upload

---
+++ timer.playground/Contents.swift
@@ -0,0 +1,12 @@
+for index in 1...6{
+     print (\(index) times 60 is \(index * 60))
+}
+
+//when tiner stops
+var timer = 180
+
+if timer == 0 {
+    print ("timer Stopped")
+} else {
+    print("keep going")
+}
